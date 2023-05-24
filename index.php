ab0970bd81a0e9cfa7a03653af133b34018a811c
Kamil Kluska <kamil6kluska7@gmail.com>
2023-05-24 20:06:47 +0200

    css

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
+
+    <?php include 'includes/head.php'; ?>
 </head>
 
 <body>
+    <?php include 'includes/header.php'; ?>
+
+    <div class="container-fluid p-0">
+        <div class="container">
+
+            <main>
+
+
+            </main>
+
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
