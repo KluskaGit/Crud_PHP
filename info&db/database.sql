fe8f692582088856197cff52ffb69d7283e248b6
Kamil Kluska <kamil6kluska7@gmail.com>
2023-05-25 23:24:25 +0200

    adding new table nad keys, some data

--- info&db/database.sql
+++ info&db/database.sql
@@ -9,14 +9,34 @@ CREATE TABLE employees
     name varchar(255),
     surname varchar(255),
     position int NOT NULL DEFAULT 1,
+    phone_number int(9),
+    email_address varchar(255),
+    city varchar(255),
+    date_of_employment DATE NOT NULL
+    
+);
+
+DROP TABLE IF EXISTS positions;
+CREATE TABLE positions
+(
+    pos_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    position_name varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+    user_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) NOT NULL,
     login varchar(255),
     password varchar(255) NOT NULL
-)
+);
 
-DROP TABLE IF EXISTS positions
-CREATE TABLE positions
-(
-    pos_id int(11) NOT NULL AUTO_INCREMENT,
-    position_name varchar(255) NOT NULL
-)
+ALTER TABLE employees
+ADD CONSTRAINT FOREIGN KEY (position) REFERENCES positions (pos_id);
+
+INSERT INTO positions(position_name) VALUES ('Intern'),('Boss');
+
+INSERT INTO employees(name, surname, position, phone_number, email_address, city, date_of_employment)
+values ('Kamil', 'Kluska', 1, 222444333, '123email@gmail.com', 'Siedlce', '2023-05-25'),
+('Mateusz', 'Kowal', 2, 121341445, '512email@gmail.com', 'Warszawa', '2023-05-25');
