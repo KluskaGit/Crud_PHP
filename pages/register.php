d7096c92d9ff58a3fc0a6f42e471e52fe1444aa5
Kamil Kluska <kamil6kluska7@gmail.com>
2023-05-26 13:24:03 +0200

    registration page

--- pages/register.php
+++ pages/register.php
@@ -10,6 +10,30 @@
 </head>
 
 <body>
+    <div class="container-fluid p-0">
+        <main class="container">
+            <div class="login_main">
+                <h1 class="log_heading">Registration</h1>
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="Email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="Email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="PasswordField" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="PasswordField" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="RepeatPasswordField" class="form-label">Repeat password</label>
+                        <input type="password" class="form-control" id="RepeatPasswordField" required>
+                    </div>
+                    <div class="mb-3 signin_bttn">
+                        <input class="whitebttn" name="register" type="submit" value="Register">
+                    </div>
+                </form>
+            </div>
+        </main>
+    </div>
 
 </body>
 
